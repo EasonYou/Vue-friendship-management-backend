4ac8be2e6238b3f8cbd1dbd020301cb122c2ad67
Eason <youyingcong@f-road.com.cn>
2017-03-09 15:55:12 +0800

    拉黑接口修改 && 修改用户信息

--- query/query.js
+++ query/query.js
@@ -1,10 +1,12 @@
 var connection = require('./connection').connection
 var utility = require('utility');
 var util = require('./util')
+var moment = require('moment')
 
 module.exports = {
 	login: function(req, res, next) {
-	connection.query("SELECT * FROM ed_admin WHERE account = '" + req.body.account +  "'", function (error, results, fields) {
+	let query = "SELECT * FROM ed_admin WHERE account = '" + req.body.account +  "'"
+	connection.query(query, function (error, results, fields) {
 			if (error) throw error;
 			if(!results.length) {
 				res.json({status: 300});
@@ -13,7 +15,8 @@ module.exports = {
 			if(results[0].password === req.body.password) {
 				var buffer = (Date.parse(new Date()) + results[0].id).toString()
 				var token = utility.md5(buffer)
-				connection.query("UPDATE ed_admin SET token = '" + token + "' WHERE id = 1" , function(error, results, fields) {
+				let query = "UPDATE ed_admin SET token = '" + token + "' WHERE id = 1"
+				connection.query(query , function(error, results, fields) {
 					if (error) throw error;
 					res.json({status: 200, message: "登陆成功", token: token});
 				})
@@ -25,7 +28,8 @@ module.exports = {
 	},
 	logout: function(req, res, next) {
 		util.checkToken(req, res, next, function() {
-			connection.query("UPDATE ed_admin SET token = '' WHERE id = 1" , function(error, results, fields) {
+			let query = "UPDATE ed_admin SET token = '' WHERE id = 1"
+			connection.query(query , function(error, results, fields) {
 				if (error) throw error;
 				res.json({status: 200, message: "登出成功"});
 			})
@@ -33,7 +37,8 @@ module.exports = {
 	},
 	getUserLists: function(req, res, next) {
 		util.checkToken(req, res, next, function() {
-			connection.query("SELECT * FROM ed_user", function(error, results, fields) {
+			let query = "SELECT * FROM ed_user"
+			connection.query(query, function(error, results, fields) {
 				if (error) throw error
 				res.json({status: 200, message: '获取列表成功', data: results})
 			})
@@ -42,18 +47,28 @@ module.exports = {
 	getUserDetail: function(req, res, next) {
 		console.log(req.body, 'gud')
 		util.checkToken(req, res, next, function() {
-			console.log('aaa')
-			connection.query("SELECT * FROM ed_user WHERE id = '" + req.body.id + "'", function(error, results, fields) {
+			let query = "SELECT * FROM ed_user WHERE id = '" + req.body.id + "'"
+			connection.query(query, function(error, results, fields) {
 				if (error) throw error
-				console.log('results')
+				results[0].birthday = moment.unix(results[0].birthday).format().slice(0, 10)
+				console.log(results[0])
 				res.json({status: 200, message: '获取用户详情成功', data: results})
 			})
 		})
 	},
 	blockUser: function(req, res, next) {
-		console.log(req.body)
 		util.checkToken(req, res, next, function() {
-			connection.query("UPDATE ed_user SET state = '" + req.body.state + "' WHERE id = " + req.body.id , function(error, results, fields) {
+			let ids = ''
+			for(let i = 0; i < req.body.ids.length; i++) {
+				ids = req.body.ids + ','
+			}
+			ids = ids.replace(/,$/, '')
+			console.log(req.body.dtime, 'dtime')
+			let query = "UPDATE ed_user SET state = '" + req.body.state 
+						+ "', dtime = '" + req.body.dtime
+						+ "' WHERE id in (" + ids + ")"
+						console.log(query)
+			connection.query(query, function(error, results, fields) {
 					if (error) throw error;
 					res.json({status: 200, message: "用户状态操作成功"});
 				})
@@ -61,13 +76,17 @@ module.exports = {
 	},
 	userDataSubmit: function(req, res, next) {
 		let q = req.body
-		console.log("UPDATE ed_user SET nickname = '" + q.nickname 
-						+ ",address = '" + q.address			
-						+ "' WHERE id = " + req.params.id)
-
-		connection.query("UPDATE ed_user SET nickname = '" + q.nickname 
-						+ ",address = '" + q.address			
-						+ "' WHERE id = " + req.params.id , function(error, results, fields) {
+		q.birthday = Date.parse(moment(q.birthday))/1000
+		var query = "UPDATE ed_user SET nickname = '" + q.nickname 
+						+ "', address = '" + q.address
+						+ "', phone = '" + q.phone
+						+ "', sex = '" + q.sex
+						+ "', state = '" + q.state
+						+ "', birthday = '" + q.birthday
+						+ "', email = '" + q.email
+						+ "' WHERE id = " + req.params.id
+			console.log(query)
+		connection.query(query , function(error, results, fields) {
 					if (error) throw error;
 					res.json({status: 200, message: "用户状态操作成功"});
 				})
