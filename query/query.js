ed0a28cfc394544f85ef4023c00f1784b8a0302c
Eason <youyingcong@f-road.com.cn>
2017-03-09 19:26:37 +0800

    封装select && update

--- query/query.js
+++ query/query.js
@@ -5,90 +5,115 @@ var moment = require('moment')
 
 module.exports = {
 	login: function(req, res, next) {
-	let query = "SELECT * FROM ed_admin WHERE account = '" + req.body.account +  "'"
-	connection.query(query, function (error, results, fields) {
+		let query = "SELECT * FROM ed_admin WHERE account = '" + req.body.account + "'"
+		connection.query(query, function(error, results, fields) {
 			if (error) throw error;
-			if(!results.length) {
-				res.json({status: 300});
+			if (!results.length) {
+				res.json({
+					status: 300
+				});
 				return;
 			}
-			if(results[0].password === req.body.password) {
+			if (results[0].password === req.body.password) {
 				var buffer = (Date.parse(new Date()) + results[0].id).toString()
 				var token = utility.md5(buffer)
 				let query = "UPDATE ed_admin SET token = '" + token + "' WHERE id = 1"
-				connection.query(query , function(error, results, fields) {
+				connection.query(query, function(error, results, fields) {
 					if (error) throw error;
-					res.json({status: 200, message: "登陆成功", token: token});
+					res.json({
+						status: 200,
+						message: "登陆成功",
+						token: token
+					});
 				})
 			} else {
-				res.json({status: 300, message: "登陆失败"});
+				res.json({
+					status: 300,
+					message: "登陆失败"
+				});
 			}
-			
+
 		});
 	},
 	logout: function(req, res, next) {
-		util.checkToken(req, res, next, function() {
-			let query = "UPDATE ed_admin SET token = '' WHERE id = 1"
-			connection.query(query , function(error, results, fields) {
-				if (error) throw error;
-				res.json({status: 200, message: "登出成功"});
-			})
+		util.update({
+			token: util.getToken(req),
+			table: 'ed_admin',
+			query: {
+				token: ''
+			},
+			target: 'id',
+			params: [1],
+			success: function(results) {
+				res.json({
+					status: 200,
+					message: "登出成功"
+				});
+			}
 		})
 	},
 	getUserLists: function(req, res, next) {
-		util.checkToken(req, res, next, function() {
-			let query = "SELECT * FROM ed_user"
-			connection.query(query, function(error, results, fields) {
-				if (error) throw error
-				res.json({status: 200, message: '获取列表成功', data: results})
-			})
+		let query = "SELECT * FROM ed_user"
+		util.select({
+			token: util.getToken(req),
+			table: 'ed_user',
+			// target: ['nickname', 'birthday'],
+			// limit: [0, 2],
+			// params: {
+			// 	nickname: ['13750051234'],
+			// 	email: ['yycanusher@126.com']
+			// },
+			success: function(results) {
+				res.json({
+					status: 200,
+					message: '获取列表成功',
+					data: results
+				})
+			}
 		})
 	},
 	getUserDetail: function(req, res, next) {
-		console.log(req.body, 'gud')
-		util.checkToken(req, res, next, function() {
-			let query = "SELECT * FROM ed_user WHERE id = '" + req.body.id + "'"
-			connection.query(query, function(error, results, fields) {
-				if (error) throw error
-				results[0].birthday = moment.unix(results[0].birthday).format().slice(0, 10)
-				console.log(results[0])
-				res.json({status: 200, message: '获取用户详情成功', data: results})
+		let query = "SELECT * FROM ed_user WHERE id = '" + req.body.id + "'"
+
+		util.query(util.getToken(req), query, function(results) {
+			results[0].birthday = moment.unix(results[0].birthday).format().slice(0, 10)
+			res.json({
+				status: 200,
+				message: '获取用户详情成功',
+				data: results
 			})
 		})
 	},
 	blockUser: function(req, res, next) {
-		util.checkToken(req, res, next, function() {
-			let ids = ''
-			for(let i = 0; i < req.body.ids.length; i++) {
-				ids = req.body.ids + ','
+		util.update({
+			token: util.getToken(req),
+			table: 'ed_user',
+			query: req.body.data,
+			target: 'id',
+			params: req.body.data.id,
+			success: function(results) {
+				res.json({
+					status: 200,
+					message: "用户状态操作成功"
+				});
 			}
-			ids = ids.replace(/,$/, '')
-			console.log(req.body.dtime, 'dtime')
-			let query = "UPDATE ed_user SET state = '" + req.body.state 
-						+ "', dtime = '" + req.body.dtime
-						+ "' WHERE id in (" + ids + ")"
-						console.log(query)
-			connection.query(query, function(error, results, fields) {
-					if (error) throw error;
-					res.json({status: 200, message: "用户状态操作成功"});
-				})
 		})
 	},
 	userDataSubmit: function(req, res, next) {
-		let q = req.body
-		q.birthday = Date.parse(moment(q.birthday))/1000
-		var query = "UPDATE ed_user SET nickname = '" + q.nickname 
-						+ "', address = '" + q.address
-						+ "', phone = '" + q.phone
-						+ "', sex = '" + q.sex
-						+ "', state = '" + q.state
-						+ "', birthday = '" + q.birthday
-						+ "', email = '" + q.email
-						+ "' WHERE id = " + req.params.id
-			console.log(query)
-		connection.query(query , function(error, results, fields) {
-					if (error) throw error;
-					res.json({status: 200, message: "用户状态操作成功"});
-				})
+		let q = req.body.data
+		q.birthday = Date.parse(moment(q.birthday)) / 1000
+		util.update({
+			token: util.getToken(req),
+			table: 'ed_user',
+			query: q,
+			target: 'id',
+			params: req.params.id,
+			success: function(results) {
+				res.json({
+					status: 200,
+					message: "用户状态操作成功"
+				});
+			}
+		})
 	}
 }
--- query/util.js
+++ query/util.js
@@ -1,9 +1,11 @@
 var connection = require('./connection').connection
 
 module.exports = {
-	checkToken: function(req, res, next, callback) {
-		console.log(req.body.token, 'CT')
-		connection.query("SELECT * FROM ed_admin WHERE token = '" + req.body.token + "'", function (error, results, fields) {
+	getToken: function(req) {
+		return req.body.token
+	},
+	checkToken: function(token, callback) {
+		connection.query("SELECT * FROM ed_admin WHERE token = '" + token + "'", function (error, results, fields) {
 			if(error) throw error
 			if(!results.length) {
 				res.json({status: 300, message: '登陆超时'})
@@ -11,5 +13,71 @@ module.exports = {
 			}
 			callback()
 		})
+	},
+	query: function(token, query, callback) {
+		this.checkToken(token, function() {
+			connection.query(query, function(error, results, fields) {
+				if(error) throw error
+				console.log('haha')
+				callback(results)
+			})
+		})
+	},
+	/*
+	* @param {Object} options 一个简单的UPDATE方法
+	* {String} token token
+	* {String} table 表名
+	* {Object} query 查询字符，不能有对象
+	* {String} target 查询目标
+	* {Array} params 目标
+	* {Function} success(results) 成功回调函数
+	*/
+	update: function(options) {	
+		let str = '',
+			q = 'UPDATE ' + options.table,
+			p = '('
+		for(key in options.query) {
+			if(key === 'id') continue
+			str = str + key + " = '" + options.query[key] + "', "
+		}
+		str = str.replace(/,\ $/, '')
+		if(typeof options.params === 'string') {
+			var params = [].push(options.params)
+		} else {
+			var params = options.params
+		}
+		console.log(params)
+		for(let i = 0; i < options.params.length; i++) {
+			p += options.params[i] + ','
+		}
+		p = p.replace(/,$/, '')
+		p += ')'
+		str = str.replace(/,$/, '')
+		q += ' SET ' + str + ' WHERE ' + options.target + ' in ' + p
+		console.log(q)
+		this.query(options.token, q, options.success)
+	},
+	select: function(options) {
+		let target = '',
+			q = 'SELECT '
+		if(options.target && options.target.constructor === Array) {
+			for(let i = 0; i < options.target.length; i ++) {
+				target += options.target[i] + ','
+			}
+		} else {
+			target = '*'
+		}
+		target = target.replace(/,$/, '')
+		q += target + ' FROM ' + options.table
+		if(options.limit) {
+			var limit = ' LIMIT '
+			for(let i = 0; i< options.limit.length; i++) {
+				limit += options.limit[i] +','
+			}
+			limit = limit.replace(/,$/, '')
+			q += limit
+		}
+		console.log(q)
+		this.query(options.token, q, options.success)
 	}
 }
